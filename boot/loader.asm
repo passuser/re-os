b3c555e64274c6fd0739e94afa64aeca416192d7
fox <goorwt@gmail.com>
2017-08-19 15:28:32 +0800

    modify loader.asm

--- boot/loader.asm
+++ boot/loader.asm
@@ -1,5 +1,29 @@
 %include "boot.inc"
 org       LOADER_BASE_ADDR
+LOADER_STACK_TOP   equ    LOADER_BASE_ADDR
+jmp loader_start
+
+;------------------------------------------构建GDT及其内部描述符------------------------------------------------------------------
+GDT_BASE:           dd             0x00000000
+                    dd             0x00000000
+CODE_DESC:          dd             0x0000ffff
+                    dd             DESC_CODE_HIGH4
+DATA_STACK_DESC:    dd             0x0000ffff
+                    dd             DESC_DATA_HIGH4
+VIDEO_DESC:         dd             0x80000007;limit = (0xbffff ~ 0xb8000)/4k = 0x7
+                    dd             DESC_VIDEO_HIGH4
+ GDT_SIZE                equ           $-GDT_BASE
+ GDT_LIMIT               equ           GDT_SIZE-1
+       times       60 db     0       ;预留描述符空位
+SELECTOR_CODE       equ     (0x0001 << 3) + TI_GDT + RPL0 
+SELECTOR_DATE       equ     (0x0002 << 3) + TI_GDT + RPL0
+SELECTOR_VIDEO      equ     (0x0003 << 3) + TI_GDT + RPL0
+
+
+gdt_ptr              dw                GDT_LIMIT
+                     dd                GDT_BASE
+  DispStr     db  "Wecome to loader!"
+ loader_star:
 ;--------------------循环实现字符串输出------------------------------------------------------------------------------------------
    mov ax,DispStr
    mov bx,ax
@@ -12,7 +36,7 @@ org       LOADER_BASE_ADDR
 Str:
    mov byte dl,[ds:bx]
    mov byte [gs:di],dl
-   mov byte [gs:di+1],0xb4
+   mov byte [gs:di+1],0xa4
    inc bx
    inc di
    inc di
@@ -26,6 +50,21 @@ mov eax,cr0
 or  eax,00000001B
 out cr0,eax                    ;cro寄存器pe位置1
 
-lgdt [gdt_ptr]
-  DispStr     db  "Wecome to loader!"
+lgdt [gdt_ptr]                 ;加载GDT
+
+
+     jmp   dword SELECTOR_CODE:p_mode_satart      ; 刷新流水线
+
+[bits 32]
+p_mode_start:
+     mov ax,SELECTOR_DATA
+     mov ds,ax
+     mov es,ax
+     mov ss,ax
+     mov esp,LOADER_STACK_TOP
+     mov ax,SELCTOR_VIDEO
+     mov gs,ax
+
+     mov byte [gs:160],'p' 
+
 jmp $
